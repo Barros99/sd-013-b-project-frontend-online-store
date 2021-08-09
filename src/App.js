c527aa0c683094b56e081e690a119442dd676dab
unknown <sergio.espp@gmail.com>
2021-08-09 17:54:04 -0300

    Requisito 3 adicionado

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import MainScreen from './components/MainScreen';
+import ShoppCart from './components/ShoppCart';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <MainScreen />
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ MainScreen } />
+          <Route path="/ShoppCart" component={ ShoppCart } />
+        </Switch>
+      </BrowserRouter>
+    </div>
 
   );
 }
--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MainScreen extends React.Component {
   render() {
@@ -12,6 +13,12 @@ class MainScreen extends React.Component {
             name="searchBar"
           />
         </label>
+        <Link
+          to="/ShoppCart"
+          data-testid="shopping-cart-button"
+        >
+          Carrinho
+        </Link>
 
       </div>
     );
