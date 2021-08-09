c047b7ff1cfb1fdbd2a536d807f5ba7b7758249f
dineinunes <nunesvieiraclaudinei@outlook.com>
2021-08-09 19:43:57 -0300

    Recupera valor da SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 
 export default class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchText: undefined,
+    };
+
+    this.captureValue = this.captureValue.bind(this);
+  }
+
+  captureValue(event) {
+    this.setState({ searchText: event.target.value });
+  }
+
   render() {
     return (
       <div>
-        <input type="text" />
+        <input type="text" onChange={ this.captureValue } />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
