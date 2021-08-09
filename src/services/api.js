2c6564c2b633a5f6401896fa12e8088d13d47e76
JoãoBDf <joaobdf@hotmail.com>
2021-08-09 16:44:42 -0300

    test

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 export async function getCategories() {
-  // Implemente aqui
+  
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  
 }
