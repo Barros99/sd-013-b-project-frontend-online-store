b6aad23c8b5c7e3eacff0caa5c30b73b5485e7f4
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2021-08-09 17:12:59 -0300

    parte do requisito 4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import Categories from './Components/Categories';
 
 function App() {
   return (
