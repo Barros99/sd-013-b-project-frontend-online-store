1ce37f7ec5c4a0e233eeb40a930d6d8b164c041b
Jonmorais <jonathan_ms15@hotmail.com>
2021-08-09 18:10:31 -0300

    Requisito 5

--- src/Pages/Initial.js
+++ src/Pages/Initial.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import Header from '../Components/Header';
 import * as api from '../services/api';
+import './initial.css';
 
 class Initial extends Component {
   constructor() {
     super();
     this.state = {
       categories: [],
+      categoryId: '',
+      query: '',
+      products: [],
     };
+
+    this.fetchProducts = this.fetchProducts.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleChangeOnInput = this.handleChangeOnInput.bind(this);
   }
 
   componentDidMount() {
     this.fetchCategories();
+  }
+
+  handleChange(event) {
+    const { value } = event.target;
+    this.setState({
+      query: value,
+    });
+  }
+
+  handleChangeOnInput(event) {
+    const { value } = event.target;
+    this.setState({
+      categoryId: value,
+    });
   }
 
   async fetchCategories() {
@@ -21,26 +43,56 @@ class Initial extends Component {
     });
   }
 
+  async fetchProducts() {
+    const { categoryId, query } = this.state;
+    const products = await api.getProductsFromCategoryAndQuery(categoryId, query);
+    const { results } = products;
+    this.setState({
+      products: results,
+    });
+  }
+
   render() {
-    const { categories } = this.state;
+    const { categories, products } = this.state;
     return (
-      <div>
-        <Header />
+      <div className="main-content">
 
-        {
-          categories.map(({ name, id }) => (
-            <div key={ id } data-testid="category">
-              <label htmlFor={ id }>
-                <input type="radio" name="category" id={ id } />
-                { name }
-              </label>
-            </div>
-          ))
-        }
+        <div className="category">
+          {
+            categories.map(({ name, id }) => (
+              <div key={ id } data-testid="category">
+                <label htmlFor={ id }>
+                  <input
+                    type="radio"
+                    name="category"
+                    id={ id }
+                    value={ id }
+                    onChange={ this.handleChangeOnInput }
+                  />
+                  { name }
+                </label>
+              </div>
+            ))
+          }
+        </div>
 
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        <div className="content">
+          <Header change={ this.handleChange } search={ this.fetchProducts } />
+          <p data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+          <div>
+            {
+              products.map(({ id, title, price, thumbnail }) => (
+                <div key={ id } data-testid="product">
+                  <img src={ thumbnail } alt={ title } />
+                  <p>{title}</p>
+                  <p>{price}</p>
+                </div>
+              ))
+            }
+          </div>
+        </div>
 
       </div>
     );
