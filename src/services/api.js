d0528a3abc917955615711f3565875e77fbc05c1
andre9653 <andresena9653@gmail.com>
2021-08-09 16:06:56 -0300

    git commit 1.1

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 export async function getCategories() {
-  // Implemente aqui
+  const requestCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = await requestCategories.json();
+  console.log(categories);
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
