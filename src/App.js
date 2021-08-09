f481a5f1c27db415dfb762d0eaf9f99d493fb202
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-08-09 16:28:06 -0300

    resolve o requisito 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { value } = target;
+    this.setState({
+      value,
+    });
+  }
+
   render() {
+    const { value } = this.state;
     return (
-      <div>
-        Cê tá bom?
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            path="/"
+            render={ () => <SearchBar value={ value } onChange={ this.handleChange } /> }
+          />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,28 @@
+// Componente feito para o Requisito 2 - realizado em grupo,
+// com todos os membros participando.
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class SearchBar extends React.Component {
+  render() {
+    const { onChange, value } = this.props;
+    return (
+      <div>
+        <input
+          onChange={ onChange }
+          value={ value }
+          type="text"
+        />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
+
+SearchBar.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
