59a797d9eb87038a2afb5fbe9b9a302429f08a96
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2021-08-09 17:01:25 -0300

    Lint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import * as api from './services/api';
 import Home from './Pages/Home';
 
 function App() {
