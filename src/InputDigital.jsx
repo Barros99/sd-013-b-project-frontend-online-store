30c1af6c3bd13afaf1eb3c2196b2745d974660d8
MilenaCLeme <milenaleme4@hotmail.com>
2021-08-09 16:21:02 -0300

    Correcão do lint

--- src/InputDigital.jsx
+++ src/InputDigital.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
 class InputDigital extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    return(
+    return (
       <from>
-        <input type="text" ></input>
-        <p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
+        <input type="text" />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
       </from>
     );
   }
