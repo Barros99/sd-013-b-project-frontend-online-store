018008c01a6cff1fbf0863bdff3d6134e0e8667b
Bernardo Raffide <raffiweed420@gmail.com>
2021-08-09 18:08:06 -0300

    requisito 3 concluido

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SearchBar from './pages/SearchBar';
 import './App.css';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route
-          exact
-          path="/"
-          component={ SearchBar }
-        />
+        <Route exact path="/" component={ SearchBar } />
+        <Route exact path="/shoppingcart" component={ ShoppingCart } />
       </Switch>
     </BrowserRouter>
   );
