41fd62641e577f47c775329ccd1e17f81efeaa60
Lucas <lucascardim777@gmail.com>
2021-08-09 16:13:01 -0300

    git ignore updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,5 @@
 export async function getCategories() {
-  // Implemente aqui
+
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
-}
+export async function getProductsFromCategoryAndQuery()
