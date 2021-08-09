d9d5fb2b34b6e28c55c791d731585b4d9bc7e797
AquilesDantas <aquilesdant@gmail.com>
2021-08-09 17:16:09 -0300

    Requisito 2 inc

---
+++ src/components/Search.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>q</div>
+    );
+  }
+}
+
+export default Search;
