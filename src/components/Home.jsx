e008570e83da4434f55551311de3fb76ad09b67e
Pedro Brito <pedroab0119@gmail.com>
2021-08-09 19:27:00 -0300

    Add Shopping Cart Svg

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Search from './Search';
-
+import logo from '../images/shopping-cart-svgrepo-com.svg';
+import { Link } from 'react-router-dom';
 class Home extends React.Component {
   constructor(props) {
     super(props);
@@ -9,7 +10,10 @@ class Home extends React.Component {
 
   render() {
     return (
-      <Search />
+      <section>
+        <Search />
+        <Link to="/cart"> <img src={logo} alt="cart icon" width="50" height="50" /> </Link>
+      </section>
     );
   }
 }
