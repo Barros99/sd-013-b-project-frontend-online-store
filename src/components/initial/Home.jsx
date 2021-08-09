93792b5dd45f89ac2f2477ae7a3e270b83c78342
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-08-09 16:22:39 -0300

    Adicionando requisito 3 e novo component do carrinho de compras

--- src/components/initial/Home.jsx
+++ src/components/initial/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
   render() {
@@ -8,6 +9,7 @@ class Home extends Component {
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
+        <Link to="/shop" data-testid="shopping-cart-button">Carrinho de Compras</Link>
       </>
     );
   }
