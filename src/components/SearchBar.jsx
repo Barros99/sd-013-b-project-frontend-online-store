1d6ceec2773e2a9bb72ddc162caa8a1595308175
Othyose <arth_nf@hotmail.com>
2021-08-09 16:11:18 -0300

    Requisito 2 Incompleto

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="search-input" data-test-id="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+        <input type="text" id="search-input" />
+        </label>
+      </form>
+    );
+  }
+}
+
+export default SearchBar;
