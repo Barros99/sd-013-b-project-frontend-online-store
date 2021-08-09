2900decc47d99e5159995bd92dfd53e4578db1b9
LucianoJunior <lucianojunior4242@gmail.com>
2021-08-09 18:52:00 -0300

    Requisito 3 e 4

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Category from './Category';
 
 export default class SearchBar extends Component {
   render() {
     return (
-      <label htmlFor="input-search-bar">
-        <h3 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h3>
-        <input
-          type="text"
-          id="input-search-bar"
-        />
+      <div>
+        <label htmlFor="input-search-bar">
+          <h3 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h3>
+          <input
+            type="text"
+            id="input-search-bar"
+          />
+        </label>
         <Link to="shoppingcart" data-testid="shopping-cart-button">
           Carrinho
         </Link>
-      </label>
+        <Category />
+      </div>
     );
   }
 }
