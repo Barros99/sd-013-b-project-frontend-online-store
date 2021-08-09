cac2d84a9bd13db8d25e096315a36805b58ae59c
Ygor Lage <75700712+Yg-or@users.noreply.github.com>
2021-08-09 16:37:35 -0300

    ajustando Header

--- src/Pages/Initial.js
+++ src/Pages/Initial.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import Header from '../Components/Header';
 
 class Initial extends Component {
   render() {
     return (
       <div>
-        <input type="text" />
+        <Header />
+
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
-        <Link
-          to="/cart"
-          data-testid="shopping-cart-button"
-        >
-          <img
-            src="https://i.pinimg.com/originals/4b/a7/d8/4ba7d8d3c62e961494a50de2f61b2cc8.jpg"
-            alt="Shopping Cart"
-            height="60px"
-          />
-        </Link>
+
       </div>
     );
   }
