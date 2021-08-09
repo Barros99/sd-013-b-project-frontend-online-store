c13d4f3ab6a29461f0ab066616e27d91223b7455
Matheus Figueiredo <mathhsoares2@live.com>
2021-08-09 15:54:06 -0300

    Requisito 1 - Implementa funções do arquivo api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
 export async function getCategories() {
-  // Implemente aqui
+  const fetched = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = await fetched.json();
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const fetched = await fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`,
+  );
+
+  const searchResults = await fetched.json();
+  return searchResults;
 }
