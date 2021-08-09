b48484bbed1ad61fe1b4dc7d31ae7d95768096a6
AC-Oliveira <allancardoso21@gmail.com>
2021-08-09 16:37:15 -0300

    Mudanças apos o termino de 1.1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,11 @@
+const url = 'https://api.mercadolibre.com/sites/MLB/categories';
 export async function getCategories() {
-  const requestCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const requestCategories = await fetch(url);
   const categories = await requestCategories.json();
   return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const searchCategories = await categoryId();
+  const categorie = searchCategories;
 }
