00f6dcf9c4e2d2a5d288c6db86ac6b9a43348944
Pedro <pedrorsehn@hotmail.com>
2021-08-09 16:37:59 -0300

    add base ex2

---
+++ src/components/MainScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MainScreen extends React.Component {
+  render() {
+    return (
+      <div>
+        Exercicios 2
+      </div>
+    );
+  }
+}
+
+export default MainScreen;
