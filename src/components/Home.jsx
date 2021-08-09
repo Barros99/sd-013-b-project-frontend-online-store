c36ce39b0b42ad840f05d6ff179136d2be147f29
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-08-09 19:33:08 -0300

    refatora o icons do cart

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { BiCartAlt } from 'react-icons/all';
+import { BiCartAlt } from 'react-icons/bi';
 
 export default class Home extends React.Component {
   render() {
