eb62cd2005c2d1f8d32bdedebca3d691c59fbbd8
André <andrebpessoa09@gmail.com>
2021-08-09 17:53:33 -0300

    Requisito 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class SearchBar extends React.Component {
   render() {
@@ -8,6 +9,7 @@ export default class SearchBar extends React.Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Link data-testid="shopping-cart-button" to="/cart">Carrinho</Link>
       </div>
     );
   }
