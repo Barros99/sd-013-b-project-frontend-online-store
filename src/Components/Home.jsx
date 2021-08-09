d60e8960c2511e80755d7236744d5df4175f09dd
Matheus <matheus.pormanhani@gmail.com>
2021-08-09 17:53:12 -0300

    Requisito 5 *in progress**

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import BarSearch from './BarSearch';
 import Category from './Category';
+import ProductList from './ProductList';
 
 class Home extends Component {
   render() {
     return (
-      <main>
-        <BarSearch />
-        <Category />
-        <Link data-testid="shopping-cart-button" to="cart/">Cart</Link>
-      </main>
+      <>
+        <header>
+          <BarSearch />
+          <Link data-testid="shopping-cart-button" to="cart/">Cart</Link>
+        </header>
+        <main>
+          <Category />
+          <ProductList />
+        </main>
+      </>
     );
   }
 }
