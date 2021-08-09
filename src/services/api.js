ab353fe1c5cdcdd92d30eee2d8034707c6ae3263
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-09 16:27:03 -0300

    Implementa getCategories

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,9 @@
 export async function getCategories() {
   // Implemente aqui
+  const urlBase = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(urlBase);
+  const productList = await response.json();
+  return productList;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
