1f8972ffe1f3f6bb926d534c24a173c682afe36f
Marcos Estevao <dusdoom@gmail.com>
2021-08-09 19:30:23 -0300

    Challenge 4 Completed

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import CategoryFilter from '../components/CategoryFilter';
 
 class ProductList extends React.Component {
   render() {
     return (
-      <h4 data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </h4>
+      <div>
+        <h4 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h4>
+        <CategoryFilter />
+      </div>
     );
   }
 }
