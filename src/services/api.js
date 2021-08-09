7908757d8c3d6a0c2b24439af39186bf23ba2fdb
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-09 17:08:44 -0300

    Correções no lint

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ export async function getCategories() {
   return data;
 }
 
-export async function getProductsFromCategoryAndQuery( categoryId, query ) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
   const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&̲q=${query}`);
   const data = await response.json();
