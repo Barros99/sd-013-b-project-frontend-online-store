ed977815e444f118e052278b468bc5a81f1ccd18
PedroSpelta <p.h.s.8989@hotmail.com>
2021-08-09 15:50:26 -0300

    realizando o segundo requisito em pair programming com os 4 participantes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import * as api from './services/api';
+import Home from './components/initial/Home';
 
 api.getCategories();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
