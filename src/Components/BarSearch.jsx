ee4c90b99daf7949dc51f51778b6a2e1eeab70a2
Joao Victor <joaogmavictor@gmail.com>
2021-08-09 15:35:19 -0300

    Concluindo Requisito 2

---
+++ src/Components/BarSearch.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class BarSearch extends Component {
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Digite algum termo de pesquisa ou escolha uma categoria."
+        />
+        <p
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+
+        </p>
+      </form>
+    );
+  }
+}
+
+export default BarSearch;
