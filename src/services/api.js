cedfa1f5ba0c66320c46f2574fc888d3f02e1394
noel-srocha <manoelrocha.neto.93@hotmail.com>
2021-08-09 15:52:43 -0300

    Challenge 1 Completed

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,11 @@
 export async function getCategories() {
   // Implemente aqui o seu código!
-  const requestCategory = await fetch();
+  const responseCategories = await (await fetch('https://api.mercadolibre.com/sites/MLB/categories')).json();
+  return responseCategories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const responseProduct = await (await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)).json();
+  return responseProduct;
 }
