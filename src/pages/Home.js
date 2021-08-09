7eddbd40d83aeb57ae1e29d5be9324548a53f2d6
Felipe Ventorim <felipeventorim11@gmail.com>
2021-08-09 17:33:54 -0300

    Requisito 4 - implementa a lista de categorias de produtos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,47 @@
 import React from 'react';
+import * as api from '../services/api';
+
+import Loading from '../components/Loading';
 import SearchBar from '../components/SearchBar';
+import CategoriesList from '../components/CategoriesList';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: '',
+      loading: true,
+    };
+
+    this.fetchCategoriesList = this.fetchCategoriesList.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCategoriesList();
+  }
+
+  fetchCategoriesList() {
+    api.getCategories().then((categories) => {
+      this.setState({
+        categories,
+        loading: false,
+      });
+    });
+  }
+
   render() {
+    const { categories, loading } = this.state;
+
+    if (loading) return <Loading />;
+
     return (
       <div>
         <SearchBar />
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
+        <CategoriesList categories={ categories } />
       </div>
     );
   }
