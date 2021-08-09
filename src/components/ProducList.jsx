bb1aff835647a4313599c19958e12d9dd4c49a62
AquilesDantas <aquilesdant@gmail.com>
2021-08-09 20:36:42 -0300

    Requisito 4 inc

---
+++ src/components/ProducList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as api from '../services/api';
+
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = (
+      { categories:[] }
+    );
+  }
+  async componentDidMount() {
+   await getCategoriesApi();
+  }
+  async getCategoriesApi() {
+    const list = await api.getCategories();
+    this.setState({
+      categories : list,
+    })
+  }
+  render() {
+    return (
+      <div>q</div>
+    );
+  }
+}
+
+export default ProductList;
