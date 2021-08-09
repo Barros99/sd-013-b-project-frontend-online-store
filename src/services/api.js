82a630477e327fe66f9ea6f4c5438798316b5fd2
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-08-09 15:54:04 -0300

    Requisito 1 Finalizado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
+// Participacao: Lucas T Franca, Lucas Cardim
+
 export async function getCategories() {
-  // Implemente aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const data = await fetch(url);
+  const categories = await data.json();
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const data = await fetch(url);
+  const categories = await data.json();
+  return categories;
 }
