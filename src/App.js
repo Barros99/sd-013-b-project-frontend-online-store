9d8a837442d85c9e6fbe679c6b0ce6c91bfadd38
andre9653 <andresena9653@gmail.com>
2021-08-09 19:10:55 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import SearchBar from './SearchBar';
+import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={ SearchBar } />
+          <Route
+            exact
+            path="/"
+            render={ () => (<SearchBar />) }
+          />
+          {/* <Route path="/" render={ () => <RenderCard products={ products } /> } /> */}
         </Switch>
       </BrowserRouter>
     );
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import RenderCard from './RenderCard';
+
+const api = require('../services/api');
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: '',
+      searchText: '',
+      products: [],
+    };
+  }
+
+  componentDidMount() {
+    api.getProductsFromCategoryAndQuery()
+      .then((products) => {
+        this.setState({ products: products.results });
+      });
+  }
+
+  handleClick = () => {
+    console.log('passei aqui');
+    const { category, searchText } = this.state;
+    if (category === '') {
+      return api.getProductsFromCategoryAndQuery('&CATEGORY_ID', searchText)
+        .then((products) => {
+          this.setState({ products: products.results });
+        });
+    }
+    return api.getProductsFromCategoryAndQuery(category, searchText)
+      .then((products) => {
+        this.setState({ products: products.results });
+      });
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+  render() {
+    const { searchText, products } = this.state;
+    return (
+      <main>
+        <label htmlFor="input-search-bar">
+          <h3 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h3>
+          <input
+            name="searchText"
+            type="text"
+            id="input-search-bar"
+            data-testid="query-input"
+            value={ searchText }
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="query-button"
+            onClick={ this.handleClick }
+          >
+            Pesquisar
+          </button>
+        </label>
+        <RenderCard products={ products } />
+      </main>
+    );
+  }
+}
