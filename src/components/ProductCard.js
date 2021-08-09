de1cce9ce0ce2f54857154581a3093e6b605f007
FilipeGuto <filipeguto95@gmail.com>
2021-08-09 20:45:03 -0300

    Challenge 5 Completed

---
+++ src/components/ProductCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ProductCard extends React.Component {
+  render() {
+    const { product } = this.props;
+    const { title, thumbnail, price } = product;
+    return (
+      <div data-testid="product">
+        <h4>{title}</h4>
+        <img src={ thumbnail } alt={ title } />
+        <p>{price}</p>
+      </div>
+    );
+  }
+}
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    title: PropTypes.string,
+    thumbnail: PropTypes.string,
+    price: PropTypes.number,
+  }).isRequired,
+};
+
+export default ProductCard;
