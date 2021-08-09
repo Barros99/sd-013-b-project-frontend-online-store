115a8474eea3eac0b8428dac40b15aee13ffaae3
Pedro Brito <pedroab0119@gmail.com>
2021-08-09 16:55:02 -0300

    Req1 100%

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,10 @@ export async function getCategories() {
   return fetchGetCategories.json();
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const fetchCategorieIdByQuery = await fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`,
+  );
+
+  return fetchCategorieIdByQuery.json();
 }
