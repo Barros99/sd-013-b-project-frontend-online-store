8767da57a8a9fc0c068849a82cb8a75168537a77
Thales <thalessilva.tv@gmail.com>
2021-08-09 18:46:46 -0300

    requisito 4

---
+++ src/components/Categories.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+// import api from '../services/api';
+import * as api from '../services/api';
+
+class Categories extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchApi();
+  }
+
+  async fetchApi() {
+    const dataCategories = await api.getCategories();
+    this.setState({
+      categories: dataCategories,
+    });
+    // console.log(dataCategories);
+  }
+
+  render() {
+    const { categories } = this.state;
+    console.log(categories);
+
+    return (
+      <div>
+        {categories.map((categorie) => (
+          <label
+            htmlFor={ `categorie${categorie.id}` }
+            data-testid="category"
+            key={ categorie.id }
+          >
+            <input type="radio" id={ `categorie${categorie.id}` } name="categorie" />
+            { categorie.name }
+            <br />
+          </label>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Categories;
