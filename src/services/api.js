f748832710478bea6bb062fab83d2dc2e8a8071a
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2021-08-09 16:28:18 -0300

    requirement 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
+const CATEGORIES = 'https://api.mercadolibre.com/sites/MLB/categories';
+const PRODUCTID = 'https://api.mercadolibre.com/sites/MLB/search?category=';
+
+// REQUISITO 1 FEITO POR TODOS VIA PAIR PROGRAMING;
+
 export async function getCategories() {
-  // Implemente aqui
+  const response = await (await fetch(CATEGORIES)).json();
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const response = await (await fetch(`${PRODUCTID}${categoryId}&q=${query}`)).json();
+  return response;
 }
