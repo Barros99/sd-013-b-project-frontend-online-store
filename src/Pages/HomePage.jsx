51ed2248279dcb16006c21b4784ee1763317db23
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-08-09 18:24:30 -0300

    test e lint ok

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import Card from '../Components/Card';
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -7,6 +8,10 @@ class HomePage extends React.Component {
     this.state = {
       categorysLoaded: false,
       categorys: [],
+      itemText: '',
+      selectCategory: '',
+      products: [],
+      productsLoad: false,
     };
   }
 
@@ -19,32 +24,78 @@ class HomePage extends React.Component {
       .then((list) => {
         this.setState({
           categorys: list.map((category) => (
-            <li
+            <button
+              type="button"
               data-testid="category"
               key={ category.id }
+              onClick={ () => {
+                this.updateSelectCategory(category.id);
+                this.renderList();
+              } }
             >
               { category.name }
-            </li>)),
+            </button>)),
           categorysLoaded: true,
         });
       });
   }
 
-  render() {
-    const { categorysLoaded, categorys } = this.state;
-    return (
+updateItemText = ({ target }) => {
+  const { value } = target;
+  this.setState({
+    itemText: value,
+  });
+}
+
+updateSelectCategory = (id) => {
+  this.setState({
+    selectCategory: id,
+  });
+}
+
+renderList = () => {
+  const { itemText, selectCategory } = this.state;
+  getProductsFromCategoryAndQuery(selectCategory, itemText)
+    .then((list) => {
+      this.setState({
+        products: list.results.map((item) => (
+          <Card
+            key={ item.id }
+            title={ item.title }
+            price={ item.price }
+            thumbnail={ item.thumbnail }
+          />)),
+        productsLoad: true,
+      });
+    });
+}
+
+render() {
+  const { categorysLoaded, categorys, products, productsLoad } = this.state;
+  return (
+    <div>
+      <p
+        data-testid="home-initial-message"
+      >
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
+      <ul>
+        { categorysLoaded ? categorys : <h2>Carregando...</h2> }
+      </ul>
       <div>
-        <p
-          data-testid="home-initial-message"
+        <input onChange={ this.updateItemText } data-testid="query-input" type="text" />
+        <button
+          onClick={ this.renderList }
+          data-testid="query-button"
+          type="button"
         >
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
-        <ul>
-          { categorysLoaded ? categorys : <h2>Carregando...</h2> }
-        </ul>
+          Buscar
+        </button>
       </div>
-    );
-  }
+      { productsLoad ? products : <h2>Carregando...</h2> }
+    </div>
+  );
+}
 }
 
 export default HomePage;
