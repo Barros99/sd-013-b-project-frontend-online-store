b4f389622281b2df5036114046352933851cb448
Luishsk93 <hyungsunkangkr@gmail.com>
2021-08-09 17:09:35 -0300

    Requisito 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,36 @@
+// Para listar as categorias disponíveis:
+// Tipo da requisição: GET
+// Endpoint: https://api.mercadolibre.com/sites/MLB/categories
+// Para buscar por itens por termo:
+// Tipo da requisição: GET
+// Parâmetro de busca $QUERY (este parâmetro deve ser substituído pelo valor do campo de busca)
+// Endpoint: https://api.mercadolibre.com/sites/MLB/search?q=$QUERY
+// Para buscar itens por categoria:
+// Tipo da requisição: GET
+// Parâmetro de busca $CATEGORY_ID (este parâmetro deve ser substituído pelo ID da categoria selecionada)
+// Endpoint: https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID
+// Para buscar itens de uma categoria por termo:
+// Tipo da requisição: GET
+// Parâmetro de busca $QUERY (este parâmetro deve ser substituído pelo valor do campo de busca)
+// Parâmetro de busca $CATEGORY_ID (este parâmetro deve ser substituído pelo ID da categoria selecionada)
+// Endpoint: https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=$QUERY
+
 export async function getCategories() {
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
   // Implemente aqui
+  const response = await fetch(url);
+  const object = await response.json();
+
+  return object;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+const categories = 'https://api.mercadolibre.com/sites/MLB/search?category=';
+
+export async function getProductsFromCategoryAndQuery(categoryId) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const response = await fetch(`${categories}${categoryId}`);
+  const object = await response.json();
+
+  return object;
 }
+/* query - verificar parametro no teste */
