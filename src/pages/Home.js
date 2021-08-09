4a8b336700cf870c4a41c513c7e80a265b906780
EddieRAM <edusacarneiro@gmail.com>
2021-08-09 18:47:09 -0300

    main-group-15-requisito-4-concluido

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import InputDigital from '../components/InputDigital';
+import Categories from '../components/Categories';
+import InputDigital from './InputDigital';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
+        <Categories />
         <InputDigital />
       </div>
     );
