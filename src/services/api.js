20af790a370df2ea33d6400d595af1a841417bd1
PedroSpelta <p.h.s.8989@hotmail.com>
2021-08-09 15:32:42 -0300

    realizando o primeiro requisito em pair programming com os 4 participantes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,14 @@
 export async function getCategories() {
   // Implemente aqui
+  const fetchData = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const data = await fetchData.json();
+  console.log(data);
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const fetchData = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const data = await fetchData.json();
+  return data;
 }
