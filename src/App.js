f87fc390d5ee804228a39b794ea50d975fa56cce
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-08-09 18:06:42 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-// commit inicial
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma
+          categoria.
+        </h3>
+      </div>
+    );
+  }
 }
 
 export default App;
