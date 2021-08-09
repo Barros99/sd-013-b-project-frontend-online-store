d0a50a1d15a1038b5e9b6de3cbd2e5819dc5e36e
Thiago <thiagodesouza.dev@gmail.com>
2021-08-09 17:28:30 -0300

    requisito 4 rederizar as categorias da api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SearchBar from './components/SearchBar';
-import './App.css';
+import Category from './components/Category';
 
+import './App.css';
 function App() {
   return (
     <div className="App">
       <SearchBar />
+      <Category />
     </div>
   );
 }
