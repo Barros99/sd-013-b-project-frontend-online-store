3cd9298887da8c0f6b19f55462d3a8b875a206ab
Henrique Cursino <henriquecursino46@gmail.com>
2021-08-09 16:28:45 -0300

    Requisito 1 - Finalizado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
-import * as api from './services/api';
+// import * as api from './services/api';
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {
-    api.getCategories();
-  }
-
   render() {
     return (
       <div className="App">
--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,8 @@ export async function getCategories() {
   return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const result = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`);
+  const productsFromCategory = await result.json();
+  return productsFromCategory;
 }
