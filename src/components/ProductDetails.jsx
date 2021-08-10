1266b6bffdc1c970fe6327ca41da093e4ff73a1b
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-08-10 11:35:13 -0300

    modifica a lista de categorias de li para input radio e coloca o status de nenhum produto encontrado no carregamento da pagina de produtos

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class ProductDetails extends React.Component {
   render() {
     return (
-      <div data-testid="product-detail-name">Detalhes do produto</div>
+      <div data-testid="product-detail-name">Detalhes dos produtos</div>
     );
   }
 }
