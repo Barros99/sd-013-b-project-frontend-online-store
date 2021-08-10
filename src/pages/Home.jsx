47d8d8d0fc06701ef80212631b44db57dc656dd8
Guilherme da Costa <guilhermedc93@gmail.com>
2021-08-10 17:04:52 -0300

    Requirement 05 complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,47 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from '../components/SearchBar';
 import Category from '../components/Category';
+import ProductsList from '../components/ProductsList';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      selectedCategory: '',
+      products: 'noSearchAlready',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ searchText: target.value });
+  }
+
+  handleRadioButton = ({ target }) => {
+    this.setState({ selectedCategory: target.value });
+  }
+
+  handleSearchButton = () => {
+    this.setState({ products: 'Carregando...' }, async () => {
+      const { selectedCategory, searchText } = this.state;
+      const data = await getProductsFromCategoryAndQuery(selectedCategory, searchText);
+      await this.setState({ products: data.results });
+    });
+  }
+
   render() {
+    const { searchText, products } = this.state;
     return (
       <div>
-        <SearchBar />
+        <SearchBar
+          handleChange={ this.handleChange }
+          searchText={ searchText }
+          handleSearchButton={ this.handleSearchButton }
+        />
         <Link to="/shoppingKart" data-testid="shopping-cart-button">Carrinho</Link>
-        <Category />
+        <Category handleRadioButton={ this.handleRadioButton } />
+        <ProductsList products={ products } />
       </div>
     );
   }
