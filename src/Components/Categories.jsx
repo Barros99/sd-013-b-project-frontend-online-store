7a7acd95e3441c09771cf5d5e8492b5e850d4f14
Lucas <lucascardim777@gmail.com>
2021-08-10 15:50:35 -0300

    requiriment 06, done. Waiting code review

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,20 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as api from '../services/api';
 
 export default class Categories extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+      selected: '',
+      filtered: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    api.getCategories().then((resp) => {
+      this.setState({
+        products: resp,
+      });
+    });
+    api.getProductsFromCategoryAndQuery(this.selected).then((resp) => {
+      this.setState({ filtered: resp.results });
+    });
+  }
+
+  handleChange({ target }) {
+    this.setState({ selected: target.value });
+  }
+
   render() {
-    const { categories } = this.props;
+    const { products, selected, filtered } = this.state;
     return (
-      <ul style={ { listStyle: 'none' } }>
-        { categories.map(({ id, name }) => (
-          <li key={ id }>
-            <label data-testid="category" htmlFor={ id }>
-              <input id={ id } type="radio" name="categories" />
-              { name }
-            </label>
-          </li>
-        )) }
-      </ul>
+      <div>
+        <ul style={ { listStyle: 'none' } }>
+          {products.map(({ id, name }) => (
+            <li key={ id }>
+              <label data-testid="category" htmlFor={ id }>
+                <input
+                  id={ id }
+                  type="radio"
+                  name="categories"
+                  onChange={ this.handleChange } 
+                />
+                {name}
+              </label>
+            </li>
+          ))}
+        </ul>
+        <div>
+          { selected }
+          { filtered.map((el) => (
+            <span key={ el.id } data-testid="product" />
+          )) }
+        </div>
+      </div>
     );
   }
 }
--- src/PageList/ProductList.jsx
+++ src/PageList/ProductList.jsx
@@ -1,35 +1,20 @@
 import React from 'react';
 import Categories from '../Components/Categories';
-import Search from '../Components/Search';
-import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import { getCategories } from '../services/api';
 
 export default class ProductList extends React.Component {
   constructor() {
     super();
     this.categoriesFetch = this.categoriesFetch.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.clickChange = this.clickChange.bind(this);
 
     this.state = {
       loading: true,
       categories: {},
-      products: [],
-      inputValue: '',
     };
   }
 
   componentDidMount() {
     this.categoriesFetch();
-  }
-
-  handleChange({ target }) {
-    this.setState({ inputValue: target.value });
-  }
-
-  async clickChange() {
-    const { inputValue } = this.state;
-    const returnFetch = await getProductsFromCategoryAndQuery(undefined, inputValue);
-    this.setState({ products: returnFetch.results, inputValue: '' });
   }
 
   categoriesFetch() {
@@ -39,36 +24,16 @@ export default class ProductList extends React.Component {
     });
   }
 
-  queryFetch(search) {
-    this.setState({ loading: true }, async () => {
-      const products = await getProductsFromCategoryAndQuery(undefined, search);
-      this.setState({ loading: false, products });
-    });
-  }
-
   render() {
-    const { loading, categories, inputValue, products } = this.state;
+    const { loading, categories } = this.state;
     const loadingComp = <h1>loading</h1>;
     return (
-      <div>
+      <>
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
         { loading ? loadingComp : <Categories categories={ categories } /> }
-        <input
-          data-testid="query-input"
-          value={ inputValue }
-          onChange={ this.handleChange }
-        />
-        <button
-          type="button"
-          data-testid="query-button"
-          onClick={ this.clickChange }
-        >
-          🔎
-        </button>
-        <Search product={ products } />
-      </div>
+      </>
     );
   }
 }
