9a950807c28386bdace23e58a649cd8621e975c3
Thaís Sampaio <thais_castrocs@outlook.com>
2021-08-10 18:16:17 -0300

    Requisito 4 Realizado

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,15 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import * as api from '../services/api';
 
-export default class CategoryList extends Component {
+class CategoryList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categoryState: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchApi();
+  }
+
+  async fetchApi() {
+    const categoriesApi = await api.getCategories();
+    this.setState({
+      categoryState: categoriesApi,
+    });
+  }
+
   render() {
+    const { categoryState } = this.state;
     return (
       <div>
         <section>
-          {api.getCategories().map({}))}
+          <ul>
+            {categoryState.map((category) => (
+              <li data-testid="category" key={ category.id }>
+                {category.name}
+              </li>))}
+          </ul>
         </section>
       </div>
     );
   }
 }
-// Commit Test
+
+export default CategoryList;
