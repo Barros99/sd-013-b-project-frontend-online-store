76609ee544d7e117ed93a570d7a18b5751c72627
Felipe Ventorim <felipeventorim11@gmail.com>
2021-08-10 17:58:53 -0300

    [Grupo 1] requisito 7 - implementa pagina Product Details

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 // import * as api from './services/api';
 import './App.css';
 import ShoppingCart from './pages/ShoppingCart';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Home } />
         <Route exact path="/shopping-cart" component={ ShoppingCart } />
+        <Route exact path="/product-details" component={ ProductDetails } />
       </Switch>
     </BrowserRouter>
   );
--- src/components/CartIcon.js
+++ src/components/CartIcon.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
 import '../App.css';
 
 class CartIcon extends React.Component {
   render() {
     return (
-      <div>
+      <Link to="/shopping-cart" data-testid="shopping-cart-button">
         <FontAwesomeIcon icon={ faCartPlus } className="shoppingCart" size="3x" />
-      </div>
+      </Link>
     );
   }
 }
