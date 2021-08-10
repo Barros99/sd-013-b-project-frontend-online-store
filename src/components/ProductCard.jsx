286ac1ae23977db3d0daec0088326ba930692fbb
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-08-10 13:32:59 -0300

    Adiciona componentes para buscar produtos

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ProductCard extends Component {
+  render() {
+    const { product } = this.props;
+    return (
+      <div data-testid="product">
+        <h4>{ product.title }</h4>
+        <img src={ product.thumbnail } alt="Imagem" />
+        <p className="movie-card-storyline">{ product.price }</p>
+      </div>
+    );
+  }
+}
+
+ProductCard.propTypes = {
+  product: PropTypes.objectOf(String).isRequired,
+};
+
+export default ProductCard;
---
+++ src/components/SearchField.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import ProductList from './ProductList';
+
+class SearchField extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchField: '',
+      products: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  async handleClick() {
+    const { searchField } = this.state;
+    const products = await getProductsFromCategoryAndQuery('MLB1196', searchField);
+    this.setState({
+      products: products.results,
+    });
+  }
+
+  render() {
+    const { searchField, products } = this.state;
+    return (
+      <div>
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.handleClick }
+        >
+          Buscar
+        </button>
+        <input
+          data-testid="query-input"
+          type="text"
+          name="searchField"
+          value={ searchField }
+          onChange={ this.handleChange }
+          placeholder="Ex: produto xxx"
+        />
+        <ProductList products={ products } />
+      </div>
+    );
+  }
+}
+
+export default SearchField;
