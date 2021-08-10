2f35d7015ab7ff4189c16243e6eb4468e5e40d54
AC-Oliveira <allancardoso21@gmail.com>
2021-08-10 17:29:08 -0300

    Requisito 8

--- src/components/RenderCard.jsx
+++ src/components/RenderCard.jsx
@@ -4,8 +4,15 @@ import { Link } from 'react-router-dom';
 
 // Fazer o loading...
 export default class RenderCard extends Component {
+  handleCartClick({ target }) {
+    const shoppingCartList = JSON.parse(localStorage.getItem('productList'));
+    shoppingCartList.push(target.name);
+    localStorage.setItem('productList', JSON.stringify(shoppingCartList));
+  }
+
   render() {
     const { products } = this.props;
+    if (!localStorage.getItem('productList')) localStorage.setItem('productList', '[]');
     return (
       <div>
         {products.map(((product) => (
@@ -18,6 +25,14 @@ export default class RenderCard extends Component {
               <img src={ product.thumbnail } alt="" />
               <p>{product.price}</p>
             </Link>
+            <button
+              type="button"
+              data-testid="product-add-to-cart"
+              onClick={ this.handleCartClick }
+              name={ JSON.stringify(product) }
+            >
+              Adcionar ao Carrinho
+            </button>
           </div>
         )))}
       </div>
