776a7f0ecf5bb553feed89ce981222106f0aed3b
Felipe Ventorim <felipeventorim11@gmail.com>
2021-08-10 20:15:06 -0300

    [Grupo 1] requisito 7 - implementa pagina Product Details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
-// import * as api from './services/api';
 import './App.css';
 import ShoppingCart from './pages/ShoppingCart';
 import ProductDetails from './pages/ProductDetails';
