bef62ea9c49d919eb5cfa2e67f7c500b8dfe9e2e
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-08-10 17:16:21 -0300

    resolve o requisito 6

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 import CreateList from './CreateList';
+import ListItems from './ListItems';
 
 class CategoryList extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       categories: [],
+      filtredItems: [],
+      selectedQry: '',
     };
+    this.handleSelectedCategory = this.handleSelectedCategory.bind(this);
+    this.fetchSelectedCategory = this.fetchSelectedCategory.bind(this);
   }
 
   componentDidMount() {
     this.fetchAPI();
+  }
+
+  handleSelectedCategory({ target }) {
+    this.fetchSelectedCategory(target.value);
+  }
+
+  async fetchSelectedCategory(param) {
+    const { selectedQry } = this.state;
+    const response = await getProductsFromCategoryAndQuery(param, selectedQry);
+    this.setState({
+      filtredItems: response.results,
+    });
   }
 
   async fetchAPI() {
@@ -26,14 +43,20 @@ class CategoryList extends React.Component {
   }
 
   render() {
-    const { categories } = this.state;
+    const { categories, filtredItems } = this.state;
     return (
       <div>
         <ul>
           {categories.map((category) => (
-            <CreateList key={ category.id } name={ category.name } />
+            <CreateList
+              onChange={ this.handleSelectedCategory }
+              key={ category.id }
+              name={ category.name }
+              id={ category.id }
+            />
           ))}
         </ul>
+        <ListItems items={ filtredItems } />
       </div>
     );
   }
--- src/components/CreateList.jsx
+++ src/components/CreateList.jsx
@@ -3,14 +3,16 @@ import PropTypes from 'prop-types';
 
 class CreateList extends React.Component {
   render() {
-    const { name } = this.props;
+    const { name, onChange, id } = this.props;
     return (
       <label htmlFor="category">
         <input
           style={ { display: 'block' } }
           name="category"
+          value={ id }
           type="radio"
           data-testid="category"
+          onChange={ onChange }
         />
         {name}
       </label>
@@ -20,6 +22,8 @@ class CreateList extends React.Component {
 
 CreateList.propTypes = {
   name: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default CreateList;
