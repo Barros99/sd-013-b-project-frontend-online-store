acb7e04c6437943d440c0b988db852dd24e316cc
Henrique Lima <hnamix@gmail.com>
2021-08-10 11:00:20 -0400

    tentativa req5

--- src/services/api.js
+++ src/services/api.js
@@ -11,22 +11,3 @@ export async function getProductsFromCategoryAndQuery(categoryId = {}, query = {
   const data = await response.json();
   return data;
 }
-
-// Para realizar essas buscas, vocês precisarão consultar os seguintes endpoints:
-
-// Para listar as categorias disponíveis:
-// Tipo da requisição: GET
-// Endpoint: https://api.mercadolibre.com/sites/MLB/categories
-// Para buscar por itens por termo:
-// Tipo da requisição: GET
-// Parâmetro de busca $QUERY (este parâmetro deve ser substituído pelo valor do campo de busca)
-// Endpoint: https://api.mercadolibre.com/sites/MLB/search?q=$QUERY
-// Para buscar itens por categoria:
-// Tipo da requisição: GET
-// Parâmetro de busca $CATEGORY_ID (este parâmetro deve ser substituído pelo ID da categoria selecionada)
-// Endpoint: https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID
-// Para buscar itens de uma categoria por termo:
-// Tipo da requisição: GET
-// Parâmetro de busca $QUERY (este parâmetro deve ser substituído pelo valor do campo de busca)
-// Parâmetro de busca $CATEGORY_ID (este parâmetro deve ser substituído pelo ID da categoria selecionada)
-// Endpoint: https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=$QUERY
