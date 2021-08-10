9575df99c72f2cf70c673002b53c042de32e7bd0
MilenaCLeme <milenaleme4@hotmail.com>
2021-08-10 14:17:05 -0300

    Adicionando as informações da pages

---
+++ src/pages/CarrinhoDeCompra.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class CarrinhoDeCompra extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      mensagem: true,
+    }
+  }
+
+  render() {
+    const { mensagem } = this.state;
+    if (mensagem) {
+      return (
+        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      )
+    }
+
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default CarrinhoDeCompra;
