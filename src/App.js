c2e3a1a10b9584d9563d0d5c93f8cbb83b01b162
Henrique Lima <hnamix@gmail.com>
2021-08-10 15:23:27 -0400

    caminhosearchbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import SearchBar from './SearchBar';
+import SearchBar from './Components/SearchBar';
 import Home from './Components/Home';
 
 class App extends React.Component {
