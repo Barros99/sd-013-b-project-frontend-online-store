56ec1e662657581f878e42d802aeefe14b52a15f
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-08-10 18:22:47 -0300

    Requisito 05 concluido

--- src/Components/SearchButton.jsx
+++ src/Components/SearchButton.jsx
@@ -1,41 +1,28 @@
 import React, { Component } from 'react';
-// qimport * as api from '../services/api';
+import PropTypes from 'prop-types';
+import * as api from '../services/api';
 
 export default class SearchButton extends Component {
   constructor() {
     super();
 
     this.handleClick = this.handleClick.bind(this);
-
-    this.state = {
-      searchResult: [],
-    };
-  }
-
-  componentDidMount() {
-    this.fetchProductsFromCategoryAndQuery();
   }
 
   async handleClick() {
-    await this.fetchProductsFromCategoryAndQuery();
     const { onButtonClick } = this.props;
-    const { searchResult } = this.state;
-    await onButtonClick(searchResult.results);
+    const resultApi = await this.fetchProductsFromCategoryAndQuery();
+    await onButtonClick(resultApi.results);
   }
 
   async fetchProductsFromCategoryAndQuery() {
     const { searchValueHome } = this.props;
-    console.log(searchValueHome);
-    const response = await (await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=${searchValueHome}`)).json();
-    // api.getProductsFromCategoryAndQuery('$CATEGORY_ID', searchValueHome);
-    this.setState({
-      searchResult: response,
-    });
-    // return response;
+    const response = await api
+      .getProductsFromCategoryAndQuery('$CATEGORY_ID', searchValueHome);
+    return response;
   }
 
   render() {
-    // const { searchValueHome } = this.props;
     return (
       <div>
         <button type="button" data-testid="query-button" onClick={ this.handleClick }>
@@ -45,3 +32,8 @@ export default class SearchButton extends Component {
     );
   }
 }
+
+SearchButton.propTypes = {
+  onButtonClick: PropTypes.func.isRequired,
+  searchValueHome: PropTypes.string.isRequired,
+};
