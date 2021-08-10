2cc6eb635b13ab69ce96af97e0e956b460d621f0
Leonardo Zaramello <62475802+LeonardoZaramello@users.noreply.github.com>
2021-08-10 18:31:46 -0300

    Arrumando o Link- Tirando o <Route/>

--- src/Components/CartButton.jsx
+++ src/Components/CartButton.jsx
@@ -5,14 +5,12 @@ export default class CartButton extends Component {
   render() {
     return (
       <div>
-        <Route>
-          <Link
-            to="/shoppingcart"
-            data-testid="shopping-cart-button"
-          >
-            Carrinho de compras
-          </Link>
-        </Route>
+        <Link
+          to="/shoppingcart"
+          data-testid="shopping-cart-button"
+        >
+          Carrinho de compras
+        </Link>
       </div>
     );
   }
