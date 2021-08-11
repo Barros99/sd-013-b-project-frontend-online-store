8843525302ed33d2ea7da1120ead8998d3d3e64b
Letícia Castro <aicitelks@gmail.com>
2021-08-10 21:50:56 -0300

    Correções linter <Home />

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -47,12 +47,13 @@ class Home extends React.Component {
 
         {
           resultadoQuery.length === 0 && message === true ? 'Produto não encontrado'
-            : resultadoQuery.map(({ id, title, price, thumbnail }) => <ProductList
-                key={ id }
-                title={ title }
-                price={ price }
-                thumbnail={ thumbnail }
-              />,)
+            : resultadoQuery.map(({ id, title, price, thumbnail }) => (
+            <ProductList
+              key={ id }
+              title={ title }
+              price={ price }
+              thumbnail={ thumbnail }
+            />))
         }
       </div>
     );
