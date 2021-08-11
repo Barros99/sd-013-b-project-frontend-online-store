c5d67b3997032593a85735169679a1b41e39a296
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2021-08-10 21:30:15 -0300

    Requisito 8, corrigindo o lint

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import ShoppingCart from './components/ShoppingCart';
 import Home from './components/Home';
-import ProductDetails from './components/ProductDetails';
+// import ProductDetails from './components/ProductDetails';
 
 import './App.css';
 
@@ -17,8 +17,10 @@ class App extends Component {
   }
 
   addToCart = (item) => {
-    this.setState((prevState) => ({ cartItems: [...prevState.cartItems, item] }));
-  }
+    this.setState((prevState) => ({
+      cartItems: [...prevState.cartItems, item],
+    }));
+  };
 
   render() {
     const { cartItems } = this.state;
@@ -28,17 +30,15 @@ class App extends Component {
           <Switch>
             <Route
               exact
-              path="/"
-              render={ (props) => (<Home { ...props } addToCart={ this.addToCart } />) }
+              path='/'
+              render={(props) => <Home {...props} addToCart={this.addToCart} />}
             />
             <Route
               exact
-              path="/shoppingcart"
-              render={ (props) => (<ShoppingCart
-                { ...props }
-                cartItems={ cartItems }
-              />) }
-
+              path='/shoppingcart'
+              render={(props) => (
+                <ShoppingCart {...props} cartItems={cartItems} />
+              )}
             />
           </Switch>
         </BrowserRouter>
